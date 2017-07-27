db60613c22506b656df694d9c145811482c38551
田中 聖斗 <tkiyoto@tkiyoto-no-MacBook-Air.local>
2017-07-27 14:16:04 +0900

    create作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,19 @@
 class TweetsController < ApplicationController
 
-def index
-  @tweets = Tweet.all
-end
+  def index
+    @tweets = Tweet.all
+  end
 
-def new
+  def new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  private
+  def tweet_params
+    params.permit(:name, :image, :text)
   end
 
 end
