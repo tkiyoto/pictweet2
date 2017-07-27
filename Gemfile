95acdf2853860cd6ca4e82d378533e1c7f4ccfb5
田中 聖斗 <tkiyoto@tkiyoto-no-MacBook-Air.local>
2017-07-27 16:17:36 +0900

    ログイン機能インストール

--- Gemfile
+++ Gemfile
@@ -47,5 +47,6 @@ group :development do
   gem 'compass-rails','~> 2.0'
   gem 'sprockets', '2.11.0'
   gem 'kaminari'
+  gem 'devise'
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   get 'tweets' => 'tweets#index'
   get 'tweets/new' => 'tweets#new'
   post 'tweets' => 'tweets#create'
