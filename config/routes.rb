1a25e5a9682094805fc78e82663d63a778f8fac9
田中 聖斗 <tkiyoto@tkiyoto-no-MacBook-Air.local>
2017-07-28 11:58:28 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-
-  root  'tweets#index'
   devise_for :users
-  get 'tweets' => 'tweets#index'
-  get 'tweets/new' => 'tweets#new'
-  post 'tweets' => 'tweets#create'
-  delete 'tweets/:id'  => 'tweets#destroy'
-  patch   'tweets/:id'  => 'tweets#update'
-  get   'tweets/:id/edit'  => 'tweets#edit'
-  get 'users/:id'   =>  'users#show'
+  root 'tweets#index'
+  resources :tweets                     #tweets_controllerに対してのresourcesメソッド
+  resources :tweets do
+    resources :comments, only: [:create]
+  end
+  resources :users, only: [:show]       #users_controllerに対してのresourcesメソッド
 end
